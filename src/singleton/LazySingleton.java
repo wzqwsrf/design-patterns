e1637156510d89799a2061e025c1c3e97ca0a9dd
wangzhenqing <wangzhenqing1008@163.com>
2015-01-03 23:06:47 +0800

    单例模式

---
+++ src/singleton/LazySingleton.java
@@ -0,0 +1,24 @@
+package singleton;
+
+/**
+ * @Author: wangzhenqing
+ * @Date: 2015年01月03日22:54:37
+ * @Description: 单例模式设计之懒汉式
+ */
+public class LazySingleton {
+    private volatile static LazySingleton lazySingleton = null;
+    private LazySingleton(){
+
+    }
+
+    public static LazySingleton getInstance(){
+        if (lazySingleton == null){
+            synchronized (LazySingleton.class){
+                if (lazySingleton == null){
+                    lazySingleton = new LazySingleton();
+                }
+            }
+        }
+        return lazySingleton;
+    }
+}
