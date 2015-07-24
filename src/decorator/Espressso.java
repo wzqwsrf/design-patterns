3827cc0ad0fe02ece9e3d731c43564585df12866
wangzhenqing <wzqwsrf@126.com>
2015-07-24 16:10:58 +0800

    add decorator

---
+++ src/decorator/Espressso.java
@@ -0,0 +1,18 @@
+package decorator;
+
+/**
+ * @author: wangzhenqing
+ * @date: 2015-07-24 15:25:30
+ * @description: 浓缩咖啡
+ */
+public class Espressso extends Beverage{
+
+    public Espressso() {
+        description = "Espressso";
+    }
+
+    @Override
+    public double cost() {
+        return 1.99;
+    }
+}
