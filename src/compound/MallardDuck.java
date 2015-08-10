36523dedcafef2abdd59f05591656039b92419fb
wangzhenqing <wzqwsrf@126.com>
2015-08-10 17:59:24 +0800

    add compound

---
+++ src/compound/MallardDuck.java
@@ -0,0 +1,13 @@
+package compound;
+
+/**
+ * @author: wangzhenqing
+ * @date: 2015-08-10 15:29:46
+ * @description: 绿头鸭
+ */
+public class MallardDuck implements Quackable{
+    @Override
+    public void quack() {
+        System.out.println("Quack");
+    }
+}
